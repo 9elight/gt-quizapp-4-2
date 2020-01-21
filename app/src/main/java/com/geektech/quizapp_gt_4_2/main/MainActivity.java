6cb3bcfbeec41db251023e3f7c0157fd22e5b53f
delight <melom9779@gmail.com>
2020-01-21 18:47:27 +0600

    Добавил bottomNavigationView

--- app/src/main/java/com/geektech/quizapp_gt_4_2/main/MainActivity.java
+++ app/src/main/java/com/geektech/quizapp_gt_4_2/main/MainActivity.java
@@ -5,21 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.geektech.quizapp_gt_4_2.R;
 import com.geektech.quizapp_gt_4_2.settings.SettingsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private MainViewModel mViewModel;
-    
+    private BottomNavigationView mBottomNavigation;
     private ViewPager mViewPager;
     private MainPagerAdapter mAdapter;
 
@@ -27,14 +30,67 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        initViews();
+        setmBottomNavigation();
         mViewModel = ViewModelProviders.of(this)
                 .get(MainViewModel.class);
 
-        mViewPager = findViewById(R.id.main_view_pager);
         mAdapter = new MainPagerAdapter(getSupportFragmentManager());
         mViewPager.setAdapter(mAdapter);
+
     }
+    private void initViews(){
+        mViewPager = findViewById(R.id.main_view_pager);
+        mBottomNavigation = findViewById(R.id.bottom_navigation);
+
+    }
+
+    private void setmBottomNavigation(){
+        mBottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.explore:
+                        mViewPager.setCurrentItem(0);
+                        return true;
+                    case R.id.profile:
+                        mViewPager.setCurrentItem(1);
+                        return true;
+                    case R.id.map:
+                        mViewPager.setCurrentItem(2);
+                        return true;
+                }
+                return false;
+            }
+        });
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        mBottomNavigation.getMenu().findItem(R.id.explore).setChecked(true);
+                        break;
+                    case 1:
+                        mBottomNavigation.getMenu().findItem(R.id.profile).setChecked(true);
+                        break;
+                    case 2:
+                        mBottomNavigation.getMenu().findItem(R.id.map).setChecked(true);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
 
 
     private class MainPagerAdapter extends FragmentPagerAdapter {
